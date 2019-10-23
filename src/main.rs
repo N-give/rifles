96fc7da7708ee3f4fca8a6231d9d5619e7287c3c
n-give <nate.givens03@gmail.com>
2019-10-23 17:08:51 -0400

    feat: get user input to move cursor
    only down works for now. need to set up to run in loop and not require
    enter after movement commands

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
 use async_std::task;
 
 mod contents;
+mod input;
 
 fn main() {
     task::block_on(async {
         contents::print_dir_contents(0)
             .await
             .expect("Failed to write directory contents");
+
+        let dir = input::move_pos().await.expect("Failed to get next position");
+
+        match dir {
+            input::Dir::DN => contents::print_dir_contents(1)
+                .await
+                .expect("Failed to write directory contents"),
+
+            _ => eprintln!("choose a different direction, dumby"),
+        }
     });
 }
