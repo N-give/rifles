726ff0ca94682825319c62925cb9ae737c99083c
Nathan Givens <nate.givens03@gmail.com>
2020-01-02 23:55:14 -0500

    feat: hide cursor while running

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
 use async_std::task;
+use async_std::io;
 use std::io::{stdin, stdout};
-// use async_std::prelude::*;
+use async_std::prelude::*;
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
+use termion::cursor;
 
 mod contents;
 
 fn main() {
     let stdin = stdin();
-    let _stdout = stdout().into_raw_mode().expect("Failed to enter raw mode");
+    let _raw_stdout = stdout().into_raw_mode().expect("Failed to enter raw mode");
+    let mut stdout = io::stdout();
 
     task::block_on(async move {
+        stdout.write_all(format!("{}", cursor::Hide).as_bytes()).await.unwrap();
+
         let mut current_position = 0;
         contents::print_dir_contents(current_position)
             .await
@@ -30,5 +35,7 @@ fn main() {
                 .await
                 .expect("Failed to write directory contents");
         }
+
+        stdout.write_all(format!("{}", cursor::Show).as_bytes()).await.unwrap();
     });
 }
