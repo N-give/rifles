9da96493744143b8c6028e63f5230ec0394e3e8d
Nathan Givens <nate.givens03@gmail.com>
2025-07-29 21:01:13 -0400

    remove async

--- src/contents.rs
+++ src/contents.rs
@@ -1,44 +1,116 @@
-use async_std::{fs, io, prelude::*};
-use termion::{cursor, clear, color};
+use std::{
+    ffi::OsString,
+    fs,
+    io::{self, Error, Write},
+};
+use termion::{clear, color, cursor};
 
-pub async fn print_dir_contents(pos: usize) -> io::Result<usize> {
-    let mut stdout = io::stdout();
-    let mut output = format!("{}{}", clear::All, cursor::Goto(1, 1));
-    let mut length: usize = 0usize;
-    let mut entries = fs::read_dir(".").await?.enumerate();
-
-    while let Some((i, entry_result)) = entries.next().await {
-        output.push_str(format_output(entry_result?, pos == i).await?.as_str());
-        length = i;
-    }
-    stdout.write_all(output.as_bytes()).await?;
-    Ok(length)
+pub enum EntryType {
+    FILE,
+    DIRECTORY,
+    SYMLINK,
 }
 
-async fn format_output(entry: fs::DirEntry, selected: bool) -> io::Result<String> {
-    let name = entry.file_name().into_string().unwrap();
-    let mut output = String::new();
-    if selected {
-        output.push_str(format!("{}", color::Bg(color::LightMagenta)).as_str());
-    } /* else {
-    } */
+pub struct Entry {
+    path: String,
+    entry_type: EntryType,
+}
 
-    let ft = entry.file_type().await?;
-    if ft.is_dir() {
-        output.push_str(format!("{}", color::Fg(color::Cyan)).as_str());
-    } else if ft.is_file() {
-        output.push_str(format!("{}", color::Fg(color::White)).as_str());
-    } else {
-        output.push_str(format!("{}", color::Fg(color::Yellow)).as_str());
-    };
+impl Entry {
+    pub fn format(&self, selected: bool) -> String {
+        let mut output = String::new();
+        if selected {
+            output.push_str(&color::Bg(color::LightMagenta).to_string());
+        }
 
-    output.push_str(
-        format!(
-            "{}{}{}\r\n",
-            name,
-            color::Fg(color::Reset),
-            color::Bg(color::Reset)
-        ).as_str()
-    );
-    Ok(output)
+        match self.entry_type {
+            EntryType::FILE => {
+                output.push_str(&color::Fg(color::White).to_string());
+            }
+            EntryType::DIRECTORY => {
+                output.push_str(&color::Fg(color::Cyan).to_string());
+            }
+            EntryType::SYMLINK => {
+                output.push_str(&color::Fg(color::Yellow).to_string());
+            }
+        }
+
+        output.push_str(&self.path);
+        output.push_str(&color::Fg(color::Reset).to_string());
+        output.push_str(&color::Bg(color::Reset).to_string());
+        output.push_str("\r\n");
+        output
+    }
+}
+
+pub struct Directory {
+    working_directory: String,
+    position: usize,
+    entries: Vec<Entry>,
+}
+
+impl Directory {
+    pub fn default() -> io::Result<Self> {
+        Ok(Directory {
+            working_directory: std::env::current_dir()?
+                .into_os_string()
+                .into_string()
+                .map_err(os_string_err)?,
+            position: 0,
+            entries: fs::read_dir(".")?
+                .map(|dr| -> Result<Entry, Error> {
+                    let d = dr?;
+                    let path = d.file_name().into_string().map_err(os_string_err)?;
+                    let file_type = d.file_type()?;
+                    let entry_type = if file_type.is_file() {
+                        EntryType::FILE
+                    } else if file_type.is_dir() {
+                        EntryType::DIRECTORY
+                    } else {
+                        EntryType::SYMLINK
+                    };
+                    Ok(Entry { path, entry_type })
+                })
+                .collect::<Result<Vec<Entry>, io::Error>>()?,
+        })
+    }
+
+    pub fn len(&self) -> usize {
+        self.entries.len()
+    }
+
+    pub fn current_working_directory(&self) -> &str {
+        &self.working_directory
+    }
+
+    pub fn current_selection(&self) -> &str {
+        &self.entries[self.position as usize].path
+    }
+
+    pub fn increase_position(&mut self) {
+        self.position = self.position.checked_add(1).unwrap_or(self.len() - 1);
+    }
+
+    pub fn decrease_position(&mut self) {
+        self.position = self.position.checked_sub(1).unwrap_or(0);
+    }
+
+    pub fn display_entries(&mut self) -> io::Result<()> {
+        let mut display_str = format!("{}{}", clear::All, cursor::Goto(1, 1));
+        for (i, entry) in self.entries.iter_mut().enumerate() {
+            display_str.push_str(entry.format(self.position as usize == i).as_str());
+            if self.position as usize == i {
+                self.working_directory = entry.path.clone();
+            }
+        }
+        io::stdout().write_all(display_str.as_bytes())?;
+        Ok(())
+    }
+}
+
+fn os_string_err(os_str: OsString) -> io::Error {
+    io::Error::new(
+        io::ErrorKind::Unsupported,
+        format!("Failed to convert OsString: {os_str:?}"),
+    )
 }
--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,59 @@
-use async_std::prelude::*;
-use async_std::{io, task};
-use std::io::{stdin, stdout};
+use std::{
+    io::{self, stdin, stdout, Write},
+    path::Path,
+};
 use termion::{clear, cursor, event::Key, input::TermRead, raw::IntoRawMode};
+
+use crate::contents::Directory;
 
 mod contents;
 
-#[async_std::main]
-async fn main() {
+fn main() -> Result<(), io::Error> {
     let stdin = stdin();
-    let _raw_stdout =
-        stdout().into_raw_mode().expect("Failed to enter raw mode");
+    let _raw_stdout = stdout().into_raw_mode().expect("Failed to enter raw mode");
     let mut stdout = io::stdout();
 
-    stdout
-        .write_all(format!("{}", cursor::Hide).as_bytes())
-        .await
-        .unwrap();
+    stdout.write_all(format!("{}", cursor::Hide).as_bytes())?;
 
-    let mut current_position = 0;
-    let mut current_length = 0;
-    contents::print_dir_contents(current_position)
-        .await
-        .expect("Failed to read directory contents");
+    let mut current_dir = Directory::default()?;
+    current_dir.display_entries()?;
 
     for key in stdin.keys() {
         match key.unwrap() {
+            // Quit and close rifles
             Key::Char('q') => break,
-            // Key::Char('h') => unimplemented!(),
-            Key::Char('j') => {
-                if current_position < current_length {
-                    current_position += 1;
-                }
+
+            // Navigate within a directory
+            Key::Char('j') => current_dir.increase_position(),
+            Key::Char('k') => current_dir.decrease_position(),
+
+            // Move up a directory
+            Key::Char('h') => {
+                std::env::set_current_dir(Path::new(".."))?;
+                current_dir = Directory::default()?;
+            }
+            // Navigate into the current directory
+            // TODO handle current path isn't a directory
+            Key::Char('l') => {
+                std::env::set_current_dir(Path::new(current_dir.current_selection()))?;
+                current_dir = Directory::default()?;
             }
 
-            Key::Char('k') => {
-                if current_position > 0 {
-                    current_position -= 1;
-                }
+            Key::Char('n') => {
+                todo!("handle creating a new path");
             }
-
-            // Key::Char('l') => unimplemented!(),
+            Key::Char('d') => {
+                todo!("handle deleting the current path");
+            }
             _ => eprintln!("Invalid Character"),
         }
 
-        current_length = contents::print_dir_contents(current_position)
-            .await
-            .expect("Failed to write directory contents");
+        current_dir.display_entries()?;
     }
 
     stdout
-        .write_all(
-            format!("{}{}{}", clear::All, cursor::Goto(1, 1), cursor::Show)
-                .as_bytes(),
-        )
-        .await
+        .write_all(format!("{}{}{}", clear::All, cursor::Goto(1, 1), cursor::Show).as_bytes())
         .unwrap();
+
+    Ok(())
 }
